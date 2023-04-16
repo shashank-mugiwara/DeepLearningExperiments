08988299e4163e94133e8527706ca461e5a58a59
shashank-mugiwara <shashank.j@creditsaison-in.com>
2023-04-16 20:27:52 +0530

    main: Updated optimizer.

--- parameter_optimizer/pytorch-parameter-optimizer.py
+++ parameter_optimizer/pytorch-parameter-optimizer.py
@@ -1,28 +1,17 @@
 import torch
 
 
-# Defining the function
 def f(x):
-    return torch.pow((x - 2.0), 2)
+    return torch.pow((x-2.0), 2)
 
 
-# Initializing the parameter
-initial_x = torch.tensor([-3.5], requires_grad=True)
-x_param = torch.nn.Parameter(initial_x)
+x_param = torch.nn.Parameter(torch.tensor([-3.5], requires_grad=True))
+sgd_optim = torch.optim.SGD([x_param], lr=0.1)
 
+for epoch in range(0, 100):
+    sgd_optim.zero_grad()
+    loss_value = f(x_param)
+    loss_value.backward()
+    sgd_optim.step()
 
-# Optimizer
-optimizer_sgd = torch.optim.SGD([x_param], lr=0.1)
-
-
-# Number of Iterations
-N_ITERATIONS = 60
-for epoch in range(60):
-    optimizer_sgd.zero_grad()
-    loss_incurred = f(x_param)
-    loss_incurred.backward()
-    optimizer_sgd.step()
-
-
-# Result
-print(x_param.data)
+print('Solution to the equation is: {}'.format(x_param.data))
